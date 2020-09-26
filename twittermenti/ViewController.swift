9bb14b3ec39924f3d7ad92bdbc46c71f079763f7
Justyna <kowalkowska.just@gmail.com>
2020-09-26 14:50:19 +0200

    Making batch predictions and added SwiftyJSON framework

--- twittermenti/ViewController.swift
+++ twittermenti/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SwifteriOS
 import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
@@ -21,11 +22,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let prediction = try! sentimentClassifier.prediction(text: "@Apple is a terrible company")
-        print(prediction.label)
-        
         swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (results, metadata) in
-       //     print(results)
+       
+            var tweets = [TweetSentimentClassifierInput]()
+            
+            for i in 0..<100 {
+                if let tweet = results[i]["full_text"].string {
+                    let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                    tweets.append(tweetForClassification)
+                }
+            }
+            
+            do {
+                let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                
+                var sentimentScore = 0
+                
+                for prediction in predictions {
+                    let sentiment = prediction.label
+                    if sentiment == "Pos" {
+                        sentimentScore = sentimentScore + 1
+                    } else if sentiment == "Neg" {
+                        sentimentScore -= 1
+                    }
+                }
+                
+                print(sentimentScore)
+                
+                if sentimentScore > 0 {
+                    print("Positive")
+                } else if sentimentScore == 0 {
+                    print("Neutral")
+                } else if sentimentScore < 0 {
+                    print("Negative")
+                }
+                
+            
+                
+                
+            } catch {
+                print("There was an error with making a prediction, \(error)")
+            }
+            
         } failure: { (error) in
             print("There was an error with Twitter API request, \(error)")
         }
