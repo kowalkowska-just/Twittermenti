9528c9073396292226c75098dc253d90db69531e
Justyna <kowalkowska.just@gmail.com>
2020-09-26 14:08:01 +0200

    Performing sentiment classification.

--- twittermenti/ViewController.swift
+++ twittermenti/ViewController.swift
@@ -7,19 +7,25 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var sentimentLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     
+    let sentimentClassifier = TweetSentimentClassifier()
+    
     let swifter = Swifter(consumerKey: SecretsKeyParser.getStringValue(forKey: "twitterConsumerKey"), consumerSecret: SecretsKeyParser.getStringValue(forKey: "twitterConsumerSecret"))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let prediction = try! sentimentClassifier.prediction(text: "@Apple is a terrible company")
+        print(prediction.label)
+        
         swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (results, metadata) in
-            print(results)
+       //     print(results)
         } failure: { (error) in
             print("There was an error with Twitter API request, \(error)")
         }
